be7741da518f1090857eb471b5b09816f93fb4e7
siddhart-singh <siddhartsingh@hotmail.com>
2023-11-30 10:04:12 +0530

    Add library class and fn

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-"use strict"
+"use strict";
+
+const myLibrary = [];
+
+function Book(name, author, read) {
+    this.name = name;
+    this.author = author;
+    this.read = read;
+}
+
+function addBookToLibrary(name, author, read) {
+    myLibrary.push(new Book(name, author, read));
+    console.log(myLibrary);
+}
+
+addBookToLibrary("test", "test", true);
