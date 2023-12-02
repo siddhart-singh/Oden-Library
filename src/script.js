5efe3f24c535bc18fe2a6140344547a027de8ad6
siddhart-singh <siddhartsingh@hotmail.com>
2023-12-02 10:12:10 +0530

    Add book display

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 "use strict";
-
 const myLibrary = [];
 const bookForm = document.querySelector(".book-form");
+const mainTable = document.querySelector(".main-table");
 
 function Book(name, author, read) {
   this.name = name;
@@ -11,13 +11,31 @@ function Book(name, author, read) {
 
 function addBookToLibrary(name, author, read) {
   myLibrary.push(new Book(name, author, read));
-  console.log(myLibrary);
+  displayBook();
 }
 
-addBookToLibrary("test", "test", true);
+function displayBook() {
+  myLibrary.forEach((book) => {
+    let tr = document.createElement("tr");
+    for (const key in book) {
+      let td = document.createElement("td");
+      td.innerText = book[key];
+      tr.appendChild(td);
+    }
+    const td = document.createElement("td");
+    const deleteBtn = document.createElement("Button");
+  deleteBtn.innerText = "DELETE";
+  deleteBtn.dataset.id = myLibrary.length;
+  deleteBtn.classList.add("delete-btn");
+    td.appendChild(deleteBtn);
+    tr.appendChild(td);
+    mainTable.appendChild(tr);
+  })
+}
 
 bookForm.addEventListener("submit", (e) => {
   e.preventDefault();
+  addBookToLibrary(e.target[1].value, e.target[2].value, e.target[3].value)
   bookForm.reset();
-  console.dir(e.target[1].value);
 });
+
