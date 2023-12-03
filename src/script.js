bfb1fbd82f4f27d8a21cb7ef7d864fbb89a9f7ea
siddhart-singh <siddhartsingh@hotmail.com>
2023-12-03 08:26:45 +0530

    Add delete button

--- src/script.js
+++ src/script.js
@@ -3,10 +3,10 @@
 let id = 0;
 const myLibrary = [];
 const bookForm = document.querySelector(".book-form");
-const mainTable = document.querySelector(".main-table");
+const mainTable = document.querySelector("tbody");
 
-function Book(id, name, author, read) {
-  this.id = id
+function Book(sno, name, author, read) {
+  this.sno = sno;
   this.name = name;
   this.author = author;
   this.read = read;
@@ -16,44 +16,49 @@ Book.prototype.bookStatus = function () {
   return (this.read = this.read === "Read" ? "Not Read" : "Read");
 };
 
-function addBookToLibrary(id, name, author, read) {
-  myLibrary.push(new Book(id, name, author, read));
-  displayBook(myLibrary[id]);
+function addBookToLibrary(sno, name, author, read) {
+  myLibrary.push(new Book(sno, name, author, read));
+  displayBook(myLibrary[myLibrary.length - 1], myLibrary.length);
 }
 
-function displayBook(book) { 
+function displayBook(book, sno) {
   let tr = document.createElement("tr");
-    for (const key of Object.keys(book)) {
-      let td = document.createElement("td");
-      td.classList.add("tbl-data");
-      if (book[key] == "Read" || book[key] == "Not Read") {
-        const statusBtn = document.createElement("Button");
-        statusBtn.innerText = book[key];
-        statusBtn.dataset.id = book.id;
-        statusBtn.classList.add("status-btn");
-        td.appendChild(statusBtn);
-      } else if (key === "id") {
-        td.innerText = myLibrary.length;
-      } else {
-        td.innerText = book[key];
-      }
-      tr.appendChild(td);
-    }
-    const td = document.createElement("td");
+  for (const key of Object.keys(book)) {
+    let td = document.createElement("td");
     td.classList.add("tbl-data");
-    const deleteBtn = document.createElement("Button");
-    deleteBtn.innerText = "DELETE";
-    deleteBtn.dataset.id = book.id;
-    deleteBtn.classList.add("delete-btn");
-    td.appendChild(deleteBtn);
+    if (book[key] == "Read" || book[key] == "Not Read") {
+      const statusBtn = document.createElement("Button");
+      statusBtn.innerText = book[key];
+      statusBtn.dataset.id = book.sno;
+      statusBtn.classList.add("status-btn");
+      td.appendChild(statusBtn);
+    } else if (key === "sno") {
+      td.innerText = sno;
+    } else {
+      td.innerText = book[key];
+    }
     tr.appendChild(td);
-    tr.classList.add("tbl-row");
-    mainTable.appendChild(tr);
+  }
+  const td = document.createElement("td");
+  td.classList.add("tbl-data");
+  const deleteBtn = document.createElement("Button");
+  deleteBtn.innerText = "DELETE";
+  deleteBtn.dataset.id = book.sno;
+  deleteBtn.classList.add("delete-btn");
+  td.appendChild(deleteBtn);
+  tr.appendChild(td);
+  tr.classList.add("tbl-row");
+  mainTable.appendChild(tr);
 }
 
 bookForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  addBookToLibrary(id++, e.target[1].value, e.target[2].value, e.target[3].value);
+  addBookToLibrary(
+    id++,
+    e.target[1].value,
+    e.target[2].value,
+    e.target[3].value,
+  );
   console.log(myLibrary[0]);
   bookForm.reset();
 });
@@ -64,8 +69,12 @@ document.addEventListener("click", (e) => {
     target.innerText = myLibrary[+target.dataset.id].bookStatus();
   }
 
-  if(e.target.closest(".delete-btn")) {
+  if (e.target.closest(".delete-btn")) {
     const target = e.target.closest(".delete-btn");
-    console.log(target)
+    myLibrary.splice(target.dataset.id, 1);
+    [...mainTable.children].forEach((child) => {
+      mainTable.removeChild(child)
+  });
+    myLibrary.forEach((book, index) => {displayBook(book, index + 1)})
   }
 });
